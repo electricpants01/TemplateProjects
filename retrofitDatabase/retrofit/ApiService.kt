206ca2c3d02044b078444e4af524845df9e941f8
Christian Torrico Avila <chtorrico@ecisolutions.com>
2022-05-09 23:03:53 -0400

    added suspend keyboard

--- retrofitDatabase/retrofit/ApiService.kt
+++ retrofitDatabase/retrofit/ApiService.kt
@@ -3,14 +3,14 @@ package com.example.retrofittemplate.retrofitDatabase.retrofit
 interface ApiService {
 
 //    @GET("users/{username}/repos")
-//    fun getRepos(@Path("username") username:String):Call<List<Repo>>
+//    suspend fun getRepos(@Path("username") username:String):Call<List<Repo>>
 //
 //    @POST("users/new")
-//    fun getRepos(@Path("username") user:User):Call<User>
+//    suspend fun getRepos(@Path("username") user:User):Call<User>
 //
 //    @PUT("users/{id}")
-//    fun getRepos(@Path("id") userId:Int):Call<User>
+//    suspend fun getRepos(@Path("id") userId:Int):Call<User>
 //
 //    @DELETE("users/{id}")
-//    fun getRepos(@Path("id") userId:Int):Call<User>
+//    suspend fun getRepos(@Path("id") userId:Int):Call<User>
 }
