dbf630c4fba5b547e11f1c019ed94ad0c3e0de06
Christian Torrico Avila <chtorrico@ecisolutions.com>
2022-05-09 22:10:23 -0400

    retrofit one directory up

---
+++ retrofitDatabase/retrofit/ApiService.kt
@@ -0,0 +1,16 @@
+package com.example.retrofittemplate.retrofitDatabase.retrofit
+
+interface ApiService {
+
+//    @GET("users/{username}/repos")
+//    fun getRepos(@Path("username") username:String):Call<List<Repo>>
+//
+//    @POST("users/new")
+//    fun getRepos(@Path("username") user:User):Call<User>
+//
+//    @PUT("users/{id}")
+//    fun getRepos(@Path("id") userId:Int):Call<User>
+//
+//    @DELETE("users/{id}")
+//    fun getRepos(@Path("id") userId:Int):Call<User>
+}
